1ce1b24602c791dd4766efee978086cc08f855a7
Mason Speck <mcs.5299@gmail.com>
2025-01-22 20:51:25 -0500

    bootstrap

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+#include "glh/classes/OpenGLApplication.h"
+
+int main()
+{
+    OpenGLApplication::ApplicationConfig appConfig;
+    appConfig.windowName = "GitRepoManager";
+    appConfig.windowInitWidth = 2000;
+    appConfig.windowInitHeight = 800;
+    appConfig.windowPosX = 100;
+    appConfig.windowPosY = 100;
+    appConfig.windowBorderless = false;
+    appConfig.windowResizeEnable = false;
+    appConfig.windowDarkmode = true;
+    appConfig.windowRounded = true;
+    appConfig.windowAlwaysOnTop = false;
+    appConfig.vsyncEnable = true;
+    appConfig.transparentFramebuffer = false;
+    appConfig.glVersionMajor = 4;
+    appConfig.glVersionMinor = 6;
+    appConfig.glslVersionString = "#version 460"; // Used for DearImgui, leave default unless you know what to put here
+    appConfig.customDrawFunc = nullptr;      // std::function<void(GLFWwindow*)>
+    appConfig.customKeyCallback = nullptr;   // std::function<void(GLFWwindow* window, int key, int scancode, int action, int mods)>
+    appConfig.customErrorCallback = nullptr; // std::function<void(int error_code, const char* description)>
+    appConfig.customDropCallback = nullptr;  // std::function<void(GLFWwindow* window, int count, const char** paths)>
+    appConfig.customPollingFunc = nullptr;   // std::function<void()>
+
+    try {
+        OpenGLApplication application(appConfig);
+    }
+    catch (const std::exception& e) {
+        std::cout << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
